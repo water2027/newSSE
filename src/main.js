b93e9810b18e6bdff1a16decb0c17d3786ad1dae
qs_theme <qstxdy@gmail.com>
2024-09-25 21:04:55 +0800

    准备添加pwa

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import './assets/msgbox.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
 
 const app = createApp(App)
 
