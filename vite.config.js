f25daabbd55d3566fa7c9099c0e57c198bbca1f1
qs_theme <qstxdy@gmail.com>
2024-10-08 00:06:21 +0800

    部署

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueJsxPlugin from '@vitejs/plugin-vue-jsx'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/',
+  base:'/base',
   plugins: [
     vue(),
     vueJsxPlugin(),
