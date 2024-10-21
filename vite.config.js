dd84cf2b8e025a8afb02d9938279d4a2f9d6cf2b
qs_theme <qstxdy@gmail.com>
2024-10-21 19:34:03 +0800

    配置PWA

--- vite.config.js
+++ vite.config.js
@@ -24,10 +24,16 @@ export default defineConfig({
         "theme_color": "#000000",
         "icons": [
           {
-            "src": "icon-192x192.png",
+            "src": "/new/android-chrome-192x192.png",
             "sizes": "192x192",
             "type": "image/png"
-          }
+          },
+          {
+            "src": "/new/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+
         ]
       },
       devOptions: {
