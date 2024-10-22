5c0da00a52620244ddb84c5691de3fb4720b83b9
qs_theme <qstxdy@gmail.com>
2024-10-22 19:29:06 +0800

    增加提示

--- src/views/OptionsView.vue
+++ src/views/OptionsView.vue
@@ -7,7 +7,7 @@
       <img
         :src="allInfo.avatarURL"
         alt="头像"
-        style="margin: auto;"
+        style="margin: auto"
       />
       <label
         for="fileInput"
@@ -17,7 +17,7 @@
         id="fileInput"
         type="file"
         accept="image/*"
-        style="display: none;"
+        style="display: none"
         @change="uploadAvatarFunc"
       />
       <div>
@@ -36,20 +36,33 @@
                 <div class="progress-container">
                   <div
                     class="progress-bar"
-                    :style="{ 
-                      width: (parseInt(allInfo.score) / parseInt(levelExpHandler(allInfo.score)) * 100) + '%',
-                      background: 'linear-gradient(to right, #ff9999, #ff4d4d)'
+                    :style="{
+                      width:
+                        (parseInt(allInfo.score) /
+                          parseInt(
+                            levelExpHandler(
+                              allInfo.score
+                            )
+                          )) *
+                        100 +
+                        '%',
+                      background:
+                        'linear-gradient(to right, #ff9999, #ff4d4d)',
                     }"
                   ></div>
                 </div>
-                {{ allInfo.score }} /{{ levelExpHandler(allInfo.score) }}
-                <span
-                  class="level level-next"
-                >{{ levelNameHandler(levelExpHandler(allInfo.score)) }}
+                {{ allInfo.score }} /{{
+                  levelExpHandler(allInfo.score)
+                }}
+                <span class="level level-next">{{
+                  levelNameHandler(
+                    levelExpHandler(allInfo.score)
+                  )
+                }}
                 </span>
               </div>
             </div>
-          </div>       
+          </div>
         </div>
         <div class="info-form">
           <div class="form-group">
@@ -80,7 +93,7 @@
           ref="VCode"
           type="text"
           placeholder="验证码"
-        >
+        />
         <button
           class="button"
           @click="codeHandler"
@@ -108,7 +121,7 @@
     <div class="data">
       <button
         class="button"
-        style="background-color: #ff4d4d;"
+        style="background-color: #ff4d4d"
         @click="logout"
       >
         退出登录
@@ -122,11 +135,15 @@ import { ref, inject, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 
 import { showMsg } from '@/components/MessageBox';
-import { levelClassHandler, levelNameHandler, levelExpHandler } from '@/utils/level';
+import {
+	levelClassHandler,
+	levelNameHandler,
+	levelExpHandler,
+} from '@/utils/level';
 
 import { sendCode } from '@/api/LoginAndRegister/utils';
 import { getAllInfo } from '@/api/info/getInfo';
-import { updateUserInfo,uploadAvatar } from '@/api/info/updateInfo';
+import { updateUserInfo, uploadAvatar } from '@/api/info/updateInfo';
 import { updatePassword } from '@/api/LoginAndRegister/forgetPwd';
 
 const router = useRouter();
@@ -138,12 +155,13 @@ const password1 = ref(null);
 const password2 = ref(null);
 
 const codeHandler = async () => {
-  try{
-    await sendCode(allInfo.value.email,1)
-  }catch(e){
-    showMsg(e)
-  }
-}
+	try {
+		await sendCode(allInfo.value.email, 1);
+		showMsg('验证码发过去啦');
+	} catch (e) {
+		showMsg(e);
+	}
+};
 
 const updatePasswordFunc = async () => {
 	if (password1.value.value === '' || password2.value.value === '') {
@@ -154,20 +172,28 @@ const updatePasswordFunc = async () => {
 		showMsg('两次密码不一致');
 		return;
 	}
-	const res = await updatePassword(
-		userInfo.value.email,
-		password1.value.value,
-		password2.value.value,
-    VCode.value.value
-	);
-	showMsg(res.msg);
+	try {
+		const res = await updatePassword(
+			userInfo.value.email,
+			password1.value.value,
+			password2.value.value,
+			VCode.value.value
+		);
+		showMsg(res.msg);
+	} catch (e) {
+		showMsg(e);
+	}
 };
 
 const uploadAvatarFunc = async (e) => {
-	const file = e.target.files[0];
-	const res = await uploadAvatar(file);
-	allInfo.value.avatarURL = res.data.avatar_url;
-	showMsg(res.msg);
+	try {
+		const file = e.target.files[0];
+		const res = await uploadAvatar(file);
+		allInfo.value.avatarURL = res.data.avatar_url;
+		showMsg(res.msg + '记得点击下面的修改信息~');
+	} catch (e) {
+		showMsg('失败了:-(');
+	}
 };
 
 /**
@@ -175,7 +201,7 @@ const uploadAvatarFunc = async (e) => {
  */
 const updateUserInfoFunc = async () => {
 	console.log(allInfo.value.name);
-	if(/^\s+|\s+$/.test(allInfo.value.name)) {
+	if (/^\s+|\s+$/.test(allInfo.value.name)) {
 		showMsg('用户名不能以空格作为开头或者结尾');
 		return;
 	}
@@ -192,15 +218,15 @@ const updateUserInfoFunc = async () => {
  * @description 删除所有信息
  */
 const logout = () => {
-  if(confirm("确定要退出登录吗")){
-    localStorage.removeItem('userInfo');
-    localStorage.removeItem('token');
-    localStorage.removeItem('email');
-    localStorage.removeItem('password');
-    localStorage.removeItem('rememberMe');
-    router.push('/');
-    window.location.reload();
-  }
+	if (confirm('确定要退出登录吗')) {
+		localStorage.removeItem('userInfo');
+		localStorage.removeItem('token');
+		localStorage.removeItem('email');
+		localStorage.removeItem('password');
+		localStorage.removeItem('rememberMe');
+		router.push('/');
+		window.location.reload();
+	}
 };
 
 onMounted(async () => {
@@ -209,9 +235,9 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-.root{
+.root {
 	color: var(--color-text);
-  width: 100%;
+	width: 100%;
 }
 .data {
 	display: flex;
@@ -232,9 +258,9 @@ img {
 	border-radius: 50%;
 }
 .custom-file-label {
-  display: inline-block;
-  padding: 6px 12px;
-  cursor: pointer;
+	display: inline-block;
+	padding: 6px 12px;
+	cursor: pointer;
 	width: 100%;
 	height: auto;
 	border: none;
@@ -250,34 +276,34 @@ input[type='file'] {
 	height: auto;
 }
 .exp-container {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 }
 .progress-container {
-  width: 60%;
-  background-color: #ede7e79a; 
-  border-radius: 20px; 
-  overflow: hidden;
-  height: 20px;
-  margin-top: 10px;
+	width: 60%;
+	background-color: #ede7e79a;
+	border-radius: 20px;
+	overflow: hidden;
+	height: 20px;
+	margin-top: 10px;
 }
 .progress-bar {
-  height: 100%;
-  border-radius: 20px;
-  transition: width 0.5s ease-in-out;
+	height: 100%;
+	border-radius: 20px;
+	transition: width 0.5s ease-in-out;
 }
 @media screen and (min-width: 768px) {
-  .root{
-    width: 40%;
-  }
+	.root {
+		width: 40%;
+	}
 	.exp-container {
-    display: flex;
-    flex-direction: row !important;
-  }
+		display: flex;
+		flex-direction: row !important;
+	}
 }
 .level {
-  font-size: 1rem;
+	font-size: 1rem;
 	margin-left: 10px;
 	border-radius: 10%;
 	padding-left: 10px;
@@ -349,7 +375,18 @@ input[type='file'] {
 	transform: rotateX(180deg);
 	position: absolute;
 	transform-origin: bottom;
-	background: linear-gradient(45deg, #ff7d7d3e, #ff5a993e, #e376e53e, #9a7ef83e, transparent, #9a7ef73e, #e376e53e, #ff5a993e, #ff7d7d3e);
+	background: linear-gradient(
+		45deg,
+		#ff7d7d3e,
+		#ff5a993e,
+		#e376e53e,
+		#9a7ef83e,
+		transparent,
+		#9a7ef73e,
+		#e376e53e,
+		#ff5a993e,
+		#ff7d7d3e
+	);
 	background-size: 300% 300%;
 	color: transparent;
 	background-clip: text;
@@ -359,15 +396,27 @@ input[type='file'] {
 .level-8 {
 	position: relative;
 	line-height: 32px;
-	text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3), 
-	-2px -2px 4px rgba(255, 255, 255, 0.3);
+	text-shadow:
+		2px 2px 4px rgba(0, 0, 0, 0.3),
+		-2px -2px 4px rgba(255, 255, 255, 0.3);
 }
 .level-8::before {
 	content: '祖师爷';
 	transform: rotateX(160deg) skew(10deg);
 	position: absolute;
 	transform-origin: bottom;
-	background: linear-gradient(45deg, #ff7d7d, #ff5a99, #e376e5, #9a7ef8, transparent, #9a7ef8, #e376e5, #ff5a99, #ff7d7d); 
+	background: linear-gradient(
+		45deg,
+		#ff7d7d,
+		#ff5a99,
+		#e376e5,
+		#9a7ef8,
+		transparent,
+		#9a7ef8,
+		#e376e5,
+		#ff5a99,
+		#ff7d7d
+	);
 	background-size: 300% 300%;
 	color: transparent;
 	background-clip: text;
@@ -376,102 +425,101 @@ input[type='file'] {
 	opacity: 0.25;
 }
 .level-next {
-	background-color: #DDDDDD;
-  color: #8E8E8E;
+	background-color: #dddddd;
+	color: #8e8e8e;
 }
 .user-info-container {
-  display: flex;
-  flex-direction: column;
-  background-color: #f9f9f9;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  padding: 20px;
-  width: 400px;
-  margin: 20px auto;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	display: flex;
+	flex-direction: column;
+	background-color: #f9f9f9;
+	border: 1px solid #ccc;
+	border-radius: 10px;
+	padding: 20px;
+	width: 400px;
+	margin: 20px auto;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 .user-details {
-  margin-top: 10px;
+	margin-top: 10px;
 }
 .user-details p {
-  margin: 5px 0;
+	margin: 5px 0;
 }
 .info-form {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin: 20px auto;
-  padding: 20px;
-  background-color: #f9f9f9;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	margin: 20px auto;
+	padding: 20px;
+	background-color: #f9f9f9;
+	border: 1px solid #ccc;
+	border-radius: 10px;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 .form-group {
-  margin-bottom: 20px;
-  width: 100%;
+	margin-bottom: 20px;
+	width: 100%;
 }
 .form-group span {
-  display: block;
-  margin-bottom: 8px;
-  font-size: 14px;
-  color: #333;
+	display: block;
+	margin-bottom: 8px;
+	font-size: 14px;
+	color: #333;
 }
 .form-group input {
-  width: 100%;
-  padding: 10px;
-  font-size: 16px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+	width: 100%;
+	padding: 10px;
+	font-size: 16px;
+	border: 1px solid #ccc;
+	border-radius: 5px;
 }
 .button {
-  width: 100%;
-  padding: 10px;
-  background-color: #24c6dc;
-  color: #fff;
-  font-size: 16px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
+	width: 100%;
+	padding: 10px;
+	background-color: #24c6dc;
+	color: #fff;
+	font-size: 16px;
+	border: none;
+	border-radius: 5px;
+	cursor: pointer;
+	transition: background-color 0.3s ease;
 }
 .button:hover {
-  background-color: #20a0b0;
+	background-color: #20a0b0;
 }
-
 
 /* 夜间模式配色方案 */
 body.dark-mode .user-info-container {
-  background-color: #2e2e2e; 
-  border: 1px solid #444; 
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3); 
+	background-color: #2e2e2e;
+	border: 1px solid #444;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
 }
 body.dark-mode .user-details p {
-  color: #f9f9f9; 
+	color: #f9f9f9;
 }
 body.dark-mode .info-form {
-  background-color: #2e2e2e; 
-  border: 1px solid #444;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+	background-color: #2e2e2e;
+	border: 1px solid #444;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
 }
 body.dark-mode .form-group span {
-  color: #f9f9f9; 
+	color: #f9f9f9;
 }
 body.dark-mode .form-group input {
-  background-color: #444;
-  color: #f9f9f9; 
-  border: 1px solid #666;
+	background-color: #444;
+	color: #f9f9f9;
+	border: 1px solid #666;
 }
 body.dark-mode .form-group input:focus {
-  border-color: #00bfff;
-  outline: none; 
-  box-shadow: 0 0 5px rgba(0, 191, 255, 0.5);
+	border-color: #00bfff;
+	outline: none;
+	box-shadow: 0 0 5px rgba(0, 191, 255, 0.5);
 }
 body.dark-mode .button {
-  background-color: #ff8c00;
-  color: #fff; 
+	background-color: #ff8c00;
+	color: #fff;
 }
 body.dark-mode .button:hover {
-  background-color: #ffb74d;
+	background-color: #ffb74d;
 }
 </style>
