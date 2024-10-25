d68c8ca4d35ce2d03b0eaa7cf83383ae2840a9a1
qs_theme <qstxdy@gmail.com>
2024-10-25 09:15:55 +0800

    cf

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueJsxPlugin from '@vitejs/plugin-vue-jsx'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/new/',
+  base:process.env.CF_PAGES ? '/' : '/new/',
   plugins: [
     vue(),
     vueJsxPlugin(),
